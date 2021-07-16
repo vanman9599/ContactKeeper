f842dc95eed2a6f3be38ee0bee4cc434e481414e
unknown <vanjordan46@Hotmail.com>
2021-07-16 04:27:02 -0700

    Navbar Home About added

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About this app</h1>
+            <p className="my-1">
+                This is an app for keeping track of contacts
+            </p>
+            <p className="bg-dark p">
+                <strong>Version: </strong>1.0.0
+            </p>
+        </div>
+    )
+}
+
+export default About
