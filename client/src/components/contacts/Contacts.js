ade67f790c34484f76ab08a9fd0eff0fe6131d91
unknown <vanjordan46@Hotmail.com>
2021-07-17 20:32:34 -0700

    ContactItem implemented

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,18 @@
+import React, { Fragment, useContext } from 'react'
+import ContactContext from '../../context/contact/ContactContext'
+import ContactItem from './ContactItem'
+
+const Contacts = () => {
+    const contactContext = useContext(ContactContext)
+
+    const { contacts } = contactContext
+    return (
+        <div>
+            <Fragment>
+                {contacts.map(contact => <ContactItem key={contact.id} contact={contact} />)}
+            </Fragment>
+        </div>
+    )
+}
+
+export default Contacts
